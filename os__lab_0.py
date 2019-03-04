12df5bd720a12794ac31ed2f668c13ce16ba1c74
ilhanbozcan <ilhanbozcann@gmail.com>
2019-03-04 17:57:01 +0300

    os__lab_0.py

---
+++ os__lab_0.py
@@ -0,0 +1,36 @@
+import os
+import datetime
+home_directory = (os.path.expanduser("~"))
+os.chdir(home_directory)
+try:
+    os.mkdir("os_lab_0")
+    print("*****    os_lab_0 has created!!!    *****")
+
+    os.chdir("os_lab_0")
+    open("1.txt", "a").close()
+    open("2.py", "a").close()
+    print("*****    1.txt and 2.py have created in os_lab_0    *****")
+
+    print("*****    Last Modified Time    *****")
+
+    for file in os.listdir(os.getcwd()):
+        file_stat = os.stat(file).st_mtime
+        file_modified = datetime.datetime.fromtimestamp(file_stat)
+        print(file , " ======== " , file_modified)
+    print("******  txt.files    *******")
+
+    for i in os.listdir(os.getcwd()):
+        if i.endswith(".txt"):
+            print(i)
+except FileExistsError:
+    print("*****     os_lab_0 already exists    *****\n*****     Delete folder and try again ******")
+
+
+
+
+
+
+
+
+
+
